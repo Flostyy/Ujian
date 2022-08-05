2badc0e6b1244e9409ede6470bef6e6b752a5f7f
lauratita <lauratita316@gmail.com>
2022-08-05 16:37:49 +0700

    header

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -3,19 +3,53 @@
 <head>
     <link rel="stylesheet" href="<?= BASE; ?>/css/style.css">
     <link rel="stylesheet" href="<?= BASE; ?>/css/bootstrap.min.css">
+    <link rel="stylesheet" href="jquery.datetimepicker.min.css" class="">
 	<!-- <link rel="stylesheet" type="text/css" href="<?= BASE; ?>/css/styles.css"> -->
 	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
 	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" /> 
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  
 	<link rel="stylesheet" href="<?= BASE; ?>/assets/plugins/sweetalert2/sweetalert2.min.css">
 	<meta naxme="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSS -->
+  <style>
+      section {
+        min-height: 500px;
+        padding-top: 10rem;
+      }
+
+      .btn {
+        border-radius: 0.50rem;
+        font-size: 1rem;
+      }
+
+      .form-control {
+        height: calc(3rem + 2px);
+        border: 2px solid #28a745;
+        border-radius: 0.50rem;
+        font-size: 1.3rem;
+      }
+
+      .header .logo{
+        color: #28a745;
+      }
+
+      body {
+        background: #f5f5f5;
+      }
+
+
+    </style>
 </head>
 <body>
-	<!-- navbar -->
-	<section class="h-100 w-100 bg-white" style="box-sizing: border-box">
-    <nav class="navbar-1-1 navbar navbar-expand-lg navbar-light p-4 px-md-4 mb-3 bg-body" style="font-family: Poppins, sans-serif">
-      <div class="container">
+  <!-- <header class="header"> -->
+
+    <!-- navbar -->
+    <section class="h-100 w-100 bg-white" style="box-sizing: border-box">
+      <nav class="navbar-1-1 navbar navbar-expand-lg navbar-light p-4 px-md-4 mb-3 bg-body" style="font-family: Poppins, sans-serif">
+        <div class="container">
         <a class="navbar-brand" href="#">
           <svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
@@ -23,14 +57,15 @@
               clip-rule="evenodd"
               d="M3.5 15.75C3.5 8.98451 8.98451 3.5 15.75 3.5H29.75C30.7165 3.5 31.5 4.2835 31.5 5.25C31.5 6.2165 30.7165 7 29.75 7H15.75C10.9175 7 7 10.9175 7 15.75V29.75C7 30.7165 6.2165 31.5 5.25 31.5C4.2835 31.5 3.5 30.7165 3.5 29.75V15.75Z"
               fill="#38d39f"
-            />
-            <path d="M10.5 17.5C10.5 13.634 13.634 10.5 17.5 10.5H31.5C35.366 10.5 38.5 13.634 38.5 17.5V31.5C38.5 35.366 35.366 38.5 31.5 38.5H17.5C13.634 38.5 10.5 35.366 10.5 31.5V17.5Z" fill="#38d39f" />
-          </svg>
-        </a>
+              />
+              <path d="M10.5 17.5C10.5 13.634 13.634 10.5 17.5 10.5H31.5C35.366 10.5 38.5 13.634 38.5 17.5V31.5C38.5 35.366 35.366 38.5 31.5 38.5H17.5C13.634 38.5 10.5 35.366 10.5 31.5V17.5Z" fill="#38d39f" />
+            </svg>
+          </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <!-- <a href="#" class="logo"> <i class=""></i> Ujian </a> -->
           <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
             <li class="nav-item">
               <a class="nav-link px-md-4 active" aria-current="page" href="<?= BASE; ?>/Guru/index">Home</a>
@@ -46,9 +81,28 @@
             </li>
           </ul>
           <div class="d-flex">
-            <a class="btn btn-get-started btn-get-started-blue text-white" id="logout" href="#">Logout</a>
+            <!-- <a class="btn btn-get-started btn-get-started-blue text-white" id="logout" href="#">Logout</a> -->
           </div>
-        </div>
+          <div class="action" onclick="menuToggle();">
+            <div class="profile">
+              <img src="../public/img/man.png" alt="">
+            </div>
+            <div class="menu">
+              <h3><?=$_SESSION['nama']?><br><br><span>Admin</span></h3>
+              <ul>
+                <li><img src="../public/img/logout.png"><a href="#" id="logout">Log Out</a></li>
+              </ul>
+            </div>
+          </div>
+          <script>
+            function menuToggle(){
+              const toggleMenu = document.querySelector('.menu')
+              toggleMenu.classList.toggle('active')
+            }
+            </script>
+          <!-- <div id="menu-btn" class="fas fa-bars"></div> -->
       </div>
-    </nav>
-  </section>
+    </div>
+  </nav>
+<!-- </section> -->
+</header>
